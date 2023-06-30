49bf63267be7000429c6ffcb91da197d9569c2b2
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-06-30 23:34:24 +0200

    test

--- env/App.vue
+++ env/App.vue
@@ -4,21 +4,27 @@ import PVButton from "./../src/components/PVButton.vue";
 </script>
 
 <template>
-  <div>
+  <div class="container mx-auto">
     <h1 class="mb-16 text-center text-4xl font-bold text-red-500">DEV ENV</h1>
 
-    <PVCard max-width="sm">
-      <template #card-title>
-        <h3>Test</h3>
-      </template>
+    <div class="grid grid-cols-2 gap-4">
+      <PVCard>
+        <template #card-title>
+          <h3>Test</h3>
+        </template>
 
-      <template #card-body>
-        <p>Das ist ein Test</p>
-      </template>
+        <template #card-body>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            a saepe nostrum quo exercitationem! Harum ea placeat, nulla ipsa
+            aspernatur minus, sunt sed odit libero quo quos hic iusto rem.
+          </p>
+        </template>
 
-      <template #card-actions>
-        <PVButton>Card Button</PVButton>
-      </template>
-    </PVCard>
+        <template #card-actions>
+          <PVButton block>Test</PVButton>
+        </template>
+      </PVCard>
+    </div>
   </div>
 </template>
