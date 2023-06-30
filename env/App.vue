c6aa0e80a28652f9a3ffc68b7e1e00a86703b602
Thomas Iseli <ti@netivity.ch>
2023-06-30 15:12:43 +0200

    added dev env

---
+++ env/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PVButton from "./../src/components/PVButton.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="mb-16 text-center text-4xl font-bold text-red-500">DEV ENV</h1>
+
+    <PVButton loading>Test</PVButton>
+  </div>
+</template>
