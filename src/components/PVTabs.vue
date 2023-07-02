3ba2942dcaff91ce4704a06d62b48d5485a98ae2
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-07-02 22:10:21 +0200

    started tabs

---
+++ src/components/PVTabs.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import PVTabsButton from "./PVTabsButton.vue";
+
+type TabItem = {
+  title: string;
+};
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0,
+  },
+  maxWidth: {
+    type: String as () => "full" | "xs" | "sm" | "md" | "lg" | "xl",
+    default: "full",
+  },
+  items: {
+    type: Array as () => TabItem[],
+    default: () => [] as TabItem[],
+  },
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const width = computed(() => {
+  if (props.maxWidth != "full") {
+    return "max-w-" + props.maxWidth;
+  } else {
+    return "w-full";
+  }
+});
+
+const activeTabIndex = ref(props.modelValue);
+
+function changeTab(index: number) {
+  activeTabIndex.value = index;
+  emits("update:modelValue", activeTabIndex.value);
+}
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-start gap-1 rounded-lg bg-neutral-100 p-1"
+    :class="[width]"
+  >
+    <PVTabsButton
+      v-for="(tab, index) in props.items"
+      :active="activeTabIndex === index"
+      @click="changeTab(index)"
+    >
+      {{ tab.title }}
+    </PVTabsButton>
+  </div>
+</template>
---
+++ src/components/PVTabsButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    class="h-8 w-full cursor-pointer justify-center whitespace-nowrap rounded-md px-3 text-sm font-medium transition-all focus:bg-white focus:shadow focus:outline-black"
+    :class="{ 'bg-white shadow': props.active }"
+  >
+    <slot />
+  </button>
+</template>
