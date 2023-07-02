97fb5e4f155111c17199d231ce87de82fdb4e4a5
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-07-02 23:18:09 +0200

    👨🏻‍🍳👌🏼🤌🏼

--- src/components/PVTabsButton.vue
+++ src/components/PVTabsButton.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import { ref } from "vue";
 const props = defineProps({
   active: {
     type: Boolean,
     default: false,
   },
 });
+
+const hover = ref(false);
 </script>
 
 <template>
   <button
     type="button"
     class="h-8 w-full cursor-pointer justify-center whitespace-nowrap rounded-md px-3 text-sm font-medium transition-all focus:bg-white focus:shadow focus:outline-black"
-    :class="{ 'bg-white shadow': props.active }"
+    :class="{ 'bg-white shadow': props.active, 'bg-neutral-200': hover }"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
   >
     <slot />
   </button>
