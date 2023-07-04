cce5df851414696d82c8a42bb205a3d52e84af33
Thomas Iseli <ti@netivity.ch>
2023-07-04 12:03:07 +0200

    exported all components

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import "./style.css";
 
 import PVButton from "./components/PVButton.vue";
 import PVLoader from "./components/PVLoader.vue";
+import PVCard from "./components/PVCard.vue";
+import PVTabs from "./components/PVTabs.vue";
+import PVTabsButton from "./components/PVTabsButton.vue";
 
-export { PVButton, PVLoader };
+export { PVButton, PVLoader, PVCard, PVTabs, PVTabsButton };
